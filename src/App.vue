b0763a30367d1393ba593e8c0f1912e3aec14951
wangtao <wangtao@bjhub.cn>
2025-04-09 14:26:20 +0800

    修改布局

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,7 @@ const defaultMarkdown = `# MD2Card
 2. 多种主题风格任你选择
 3. 长文自动拆分，或者根据 markdown \`---\` 横线拆分
 4. 可以复制图片到\`剪贴板\`，或者下载为\`PNG\`、\`SVG\`图片
-5. 所见即所得
-6. **全新日本杂志风格**主题`
+5. 所见即所得`
 
 const markdownContent = ref('')
 const fileInput = ref(null)
